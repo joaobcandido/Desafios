ba70841baadb4bd5c7e35281e9f9b40e8ea8bba3
joao candido <joan.jb56@gmail.com>
2024-10-10 19:48:37 -0300

    added application menu

--- src/SistemaBancario.java
+++ src/SistemaBancario.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class SistemaBancario {
     public static void main(String[] args) {
 
+    int opcao = 0;
+    Scanner leitura = new Scanner(System.in);
+    String menu = """
+            *****************************
+            1 - saldo
+            2 - deposito
+            3 - saque
+            4 - sair
+            """;
 
-
-
+    while (opcao != 4){
+        System.out.println(menu);
+        System.out.print("digite uma opção: ");
+        opcao = leitura.nextInt();
+    }
 
 
     }
