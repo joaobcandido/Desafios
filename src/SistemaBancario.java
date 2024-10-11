829eb843656c3b25a081d77886949347da468939
joao candido <joan.jb56@gmail.com>
2024-10-10 21:19:20 -0300

    added options in the while loop

--- src/SistemaBancario.java
+++ src/SistemaBancario.java
@@ -3,22 +3,44 @@ import java.util.Scanner;
 public class SistemaBancario {
     public static void main(String[] args) {
 
-    int opcao = 0;
-    Scanner leitura = new Scanner(System.in);
-    String menu = """
-            *****************************
-            1 - saldo
-            2 - deposito
-            3 - saque
-            4 - sair
-            """;
+        int opcao = 0;
+        double saldo = 0;
+        double deposito = 0;
+        double saque = 0;
+        Scanner leitura = new Scanner(System.in);
+        String menu = """
+                ********************
+                1 - saldo
+                2 - deposito
+                3 - saque
+                4 - sair
+                """;
 
-    while (opcao != 4){
-        System.out.println(menu);
-        System.out.print("digite uma opção: ");
-        opcao = leitura.nextInt();
-    }
+        while (opcao != 4){
+            System.out.println(menu);
+            System.out.print("digite uma opção: ");
+            opcao = leitura.nextInt();
+            if (opcao == 1){
+                System.out.println("saldo = " + saldo);
+            }else if (opcao == 2) {
+                System.out.print("quanto voçê quer depositar: ");
+                deposito = leitura.nextDouble();
+                saldo += deposito;
+                System.out.println("deposito de " + deposito + " realizado com sucesso!!!");
+            }else if (opcao == 3) {
+                System.out.print("quanto voçê quer sacar: ");
+                saque = leitura.nextDouble();
+                if (saque < saldo){
+                    System.out.println("saque de " + saque + " realizado com sucesso!!");
+                    saldo -= saque;
+                }else {
+                    System.out.println("saldo insuficiente");
+                }
+            }else {
+                System.out.println("saindo da aplicação.....");
+            }
 
+        }
 
     }
 }
